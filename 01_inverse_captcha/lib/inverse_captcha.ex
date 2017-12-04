0a4dfb9fd0fddf08c0cf272a226d665bf750986f
Kerry Buckley <kerryjbuckley@gmail.com>
2017-12-04 16:42:05 +0000

    Solved day 1 part 2 (hadn't noticed the part twos!)

--- 01_inverse_captcha/lib/inverse_captcha.ex
+++ 01_inverse_captcha/lib/inverse_captcha.ex
@@ -28,4 +28,41 @@ defmodule InverseCaptcha do
   end
 
   defp copy_first_to_end([h|t]), do: [h|t] ++ [h]
+
+  @doc """
+  ## Examples
+
+      iex> InverseCaptcha.sum2 "1212"
+      6
+      iex> InverseCaptcha.sum2 "1221"
+      0
+      iex> InverseCaptcha.sum2 "123425"
+      4
+      iex> InverseCaptcha.sum2 "12131415"
+      4
+
+  """
+  def sum2(input) do
+    with digits <- digits(input),
+         shifted_digits <- shift(digits) do
+      digits
+      |> Enum.zip(shifted_digits)
+      |> Enum.filter(fn {a, b} -> a == b end)
+      |> Enum.map(fn {a, _} -> a end)
+      |> Enum.sum
+    end
+  end
+
+  defp digits(str) do
+    str
+    |> String.codepoints
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp shift(digits) do
+    digits
+    |> Stream.cycle
+    |> Stream.drop(length(digits) / 2)
+    |> Enum.take(length digits)
+  end
 end
