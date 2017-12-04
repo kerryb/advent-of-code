a4bbb6e3676c7aa1812bff8626fdb3df6dbf1af6
Kerry Buckley <kerryjbuckley@gmail.com>
2017-12-04 22:06:30 +0000

    Solved day 3 part 2

--- 03_spiral_memory/lib/spiral_memory.ex
+++ 03_spiral_memory/lib/spiral_memory.ex
@@ -24,12 +24,12 @@ defmodule SpiralMemory do
   end
 
   defp square_distances do
-    ever_increasing_radii()
+    ever_increasing_numbers()
     |> Stream.map(&distances_for_squares_on_a_side/1)
     |> Stream.flat_map(&repeat_for_four_sides/1)
   end
 
-  defp ever_increasing_radii, do: Stream.iterate 0, &(&1 + 1)
+  defp ever_increasing_numbers, do: Stream.iterate 0, &(&1 + 1)
 
   defp distances_for_squares_on_a_side(0),  do: [0]
   defp distances_for_squares_on_a_side(radius) do
@@ -42,4 +42,54 @@ defmodule SpiralMemory do
     |> List.duplicate(4)
     |> List.flatten
   end
+
+  # ===== PART TWO =====
+
+  @doc """
+  ## Examples
+
+      iex> SpiralMemory.first_value_larger_than 1
+      2
+      iex> SpiralMemory.first_value_larger_than 5
+      10
+      iex> SpiralMemory.first_value_larger_than 100
+      122
+  """
+  def first_value_larger_than(limit) do
+    nil
+    |> Stream.iterate(&(&1))
+    |> Stream.scan({1, {0, 0}, %{{0, 0} => 1}}, &add_to_grid/2)
+    |> Stream.drop_while(fn {value, _, _} -> value <= limit end)
+    |> Enum.take(1)
+    |> extract_value
+  end
+
+  defp add_to_grid(_, {_last_value, last_position, grid}) do
+    position = next_position last_position, grid
+    value = sum_of_neighbours position, grid
+    new_grid = Map.put(grid, position, value)
+    {value, position, new_grid}
+  end
+
+  defp next_position({x, y}, grid) do
+    n = is_nil grid[{x, y + 1}]
+    e = is_nil grid[{x + 1, y}]
+    s = is_nil grid[{x, y - 1}]
+    w = is_nil grid[{x - 1, y}]
+    case [n, e, s, w] do
+      [true, true, _, false] -> {x, y + 1}
+      [true, _, false, true] -> {x - 1, y}
+      [_, false, true, true] -> {x, y - 1}
+      [_, true, true, _] -> {x + 1, y}
+    end
+  end
+
+  defp sum_of_neighbours({x, y}, grid) do
+    for xx <- [x - 1, x, x + 1], yy <- [y - 1, y, y + 1] do
+      Map.get grid, {xx, yy}, 0
+    end
+    |> Enum.sum
+  end
+
+  defp extract_value([{value, _, _}]), do: value
 end
